916a40814e8e538e4eafbec8b97fab4c04f81169
Michael Sogomonyan <ms.sogomonyan@gmail.com>
2018-09-12 19:03:40 +0500

    add initial structure

---
+++ setup.py
@@ -0,0 +1,34 @@
+from setuptools import find_namespace_packages, setup
+
+with open('README.md', 'r') as readme:
+    long_description = readme.read()
+
+tests_dependencies = ['pytest', 'pytest-cov']
+
+setup(
+    name='kontur_package_publisher',
+    version='1.0.0',
+    description='TODO: description',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    python_requires='>=3.6',
+    install_requires=[],
+    setup_requires=['pytest-runner'],
+    tests_require=tests_dependencies,
+    extras_require={
+        'test': tests_dependencies,
+    },
+    url='https://git.skbkontur.ru/custom_dev/kontur_package_publisher',
+    packages=find_namespace_packages(include=['kontur.*']),
+    namespace_packages=['kontur'],
+    entry_points={
+        'console_scripts': ['publish=kontur.packagepublisher.api:publish'],
+    },
+    maintainer='kontur custom',
+    maintainer_email='custom@skbkontur.ru',
+    classifiers=[
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Operating System :: OS Independent'
+    ]
+)
