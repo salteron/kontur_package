1afe0b6a42701a388be8b4c305f3e2d6a3f558b0
Michael Sogomonyan <ms.sogomonyan@gmail.com>
2018-09-13 15:00:53 +0500

    fixme

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open('README.md', 'r') as readme:
 tests_dependencies = ['pytest', 'pytest-cov']
 
 setup(
-    name='kontur_package_publisher',
+    name='kontur_package',
     version='1.0.0',
     description='TODO: description',
     long_description=long_description,
@@ -18,11 +18,14 @@ setup(
     extras_require={
         'test': tests_dependencies,
     },
-    url='https://git.skbkontur.ru/custom_dev/kontur_package_publisher',
+    url='https://git.skbkontur.ru/custom_dev/kontur_package',
     packages=find_namespace_packages(include=['kontur.*']),
     namespace_packages=['kontur'],
     entry_points={
-        'console_scripts': ['publish=kontur.packagepublisher.api:publish']
+        'console_scripts': [
+            'kontur-package-release=kontur.package.api:release',
+            'kontur-package-released=kontur.package.api:released'
+        ]
     },
     maintainer='kontur custom',
     maintainer_email='custom@skbkontur.ru',
